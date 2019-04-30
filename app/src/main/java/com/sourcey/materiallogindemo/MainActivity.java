f50bb6ce54e7922742ed81c5d1668d5823763c4c
ryank262 <36464202+ryank262@users.noreply.github.com>
2019-04-30 15:50:02 -0700

    Added Some Basic Buttons to go to new dot and dot detail

--- app/src/main/java/com/sourcey/materiallogindemo/MainActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/MainActivity.java
@@ -2,17 +2,42 @@ package com.sourcey.materiallogindemo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton new_dot_btn;
+    Button dot_detail_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new_dot_btn = (FloatingActionButton) findViewById(R.id.new_btn);
+        dot_detail_btn = (Button) findViewById(R.id.detail_btn);
+
+        new_dot_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewDotActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        dot_detail_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DotDetailActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
         Intent intent = new Intent(this, LoginActivity.class);
